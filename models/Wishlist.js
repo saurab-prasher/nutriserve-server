c0e21c56d947f390e2c0eca0b66f326e594f6694
chandrika <chandrikakvenu@gmail.com>
2024-03-08 22:18:39 -0500

    wishlist method

--- models/Wishlist.js
+++ models/Wishlist.js
@@ -1,14 +1,9 @@
 const mongoose = require("mongoose");
 
-const wishlistSchema = new mongoose.Schema(
-  {
-    user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-    email: { type: String },
-    // Reference to the user who owns the wishlist
-    likedMeals: [{ type: mongoose.Schema.Types.ObjectId, ref: "Meal" }], // Array of references to the liked meals
-  },
-  { timestamps: true }
-);
+const wishlistSchema = new mongoose.Schema({
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  mealId: [{ type: mongoose.Schema.Types.ObjectId, ref: "Meal" }],
+});
 
 const Wishlist = mongoose.model("Wishlist", wishlistSchema);
 
