a5d832033fc88043b947fb7e22b42deb8848a2ef
Saurab Prasher <saurab015@gmail.com>
2024-03-09 20:29:09 -0500

    updated for vercel

--- routes/pricingRouter.js
+++ routes/pricingRouter.js
@@ -2,9 +2,10 @@ const express = require("express");
 const Pricing = require("../models/pricingPlan"); // Update the path to where your model is located
 const router = express.Router();
 
-router.get("/pricing", async (req, res) => {
+router.get("/", async (req, res) => {
   try {
     const pricingPlans = await Pricing.find();
+    console.log(pricingPlans);
     res.json(pricingPlans);
   } catch (error) {
     res.status(500).send(error.toString());
